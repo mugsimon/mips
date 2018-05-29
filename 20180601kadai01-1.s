89ab2fb9c07a3ef76e632d563bcbf14883dbe9a8
mugsimon <mug24375826dev@gmail.com>
2018-05-29 20:17:00 +0900

    kadai01-1

---
+++ 20180601kadai01-1.s
@@ -0,0 +1,26 @@
+	.data
+prmpt1:	.asciiz	"x = "
+ans:	.asciiz	"20x+3 = "
+newln:	.asciiz "\n"
+	.text
+main:
+	li	$v0, 4
+	la	$a0, prmpt1
+	syscall
+	li	$v0, 5
+	syscall
+	move 	$t0, $v0
+	li	$t2, 20
+loop:	add	$t1, $t1, $t0
+	sub	$t2, $t2, 1
+	bne	$t2, $zero, loop
+	add	$t1, $t1, 3
+	li	$v0, 4
+	la	$a0, ans
+	li	$v0, 1
+	move	$a0, $t1
+	syscall
+	li	$v0, 4
+	la	$a0, newln
+	jr	$ra
+	
